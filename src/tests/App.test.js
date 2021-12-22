b62da4d4e9c1273d7674857d6d299fbffe1c5bee
Wasan <wsulaima@jaguarlandrover.com>
2021-12-22 21:19:59 +0000

    install and config jest and enzyme

---
+++ src/tests/App.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from '../App';
+
+let wrapped = shallow(<App />);
+
+describe('App', () => {
+  it('should render the Main Page', () => {   
+    expect(wrapped).toBeTruthy();
+  });
+
+});
